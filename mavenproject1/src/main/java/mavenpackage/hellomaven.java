71ed8d001d1a6f3754eded58289b273d3ecb4b67
Sulfath Roseen <Sulfath Roseen@LAPTOP-VT3SL9S3>
2024-08-09 10:48:35 +0530

    myfirstpushing

---
+++ mavenproject1/src/main/java/mavenpackage/hellomaven.java
@@ -0,0 +1,11 @@
+package mavenpackage;
+
+public class hellomaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="Hello";
+System.out.println(a);
+	}
+
+}
